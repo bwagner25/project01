35174937fb98f972e0bee7b164a5fc87a62e498c
bwagner25 <128878@swcsd.us>
2024-02-06 13:22:05 -0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-
 import static java.lang.Integer.parseInt;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
         ArrayList<String> tasks = new ArrayList<>();
         System.out.println("\nWhat would you like to do?\n(1) Add a task.\n(2) Remove a task.\n(3) Update a task.\n(4) List all tasks.\n(0) Exit.\n");
         String userInput = input.nextLine();
@@ -29,14 +26,12 @@ public class Main {
         }
     }
     static ArrayList<String> addTask(ArrayList a){
-        Scanner input = new Scanner(System.in);
         System.out.println("What task would you like to add?");
         String newTask = input.nextLine();
         a.add(newTask);
         return a;
     }
     static ArrayList<String> removeTask(ArrayList a){
-        Scanner input = new Scanner(System.in);
         System.out.println("Which number task would you like to remove?");
         String taskNumber = input.nextLine();
         int taskIndex = parseInt(taskNumber) - 1;
@@ -44,7 +39,6 @@ public class Main {
         return a;
     }
     static ArrayList updateTask(ArrayList a){
-        Scanner input = new Scanner(System.in);
         System.out.println("Which number task would you like to update?");
         String taskNumber = input.nextLine();
         int taskIndex = parseInt(taskNumber) - 1;
